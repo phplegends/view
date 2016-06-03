029064b4dab440d5316cc5631429dbf63accbcba
Wallace de Souza Vizerra <wallacemaxters@gmail.com>
2016-06-03 09:57:38 -0300

    Adding Factory.php

--- src/Factory.php
+++ src/Factory.php
@@ -5,20 +5,100 @@ namespace PHPLegends\View;
 class Factory
 {
 
-    public function __construct($path, $extension = 'php')
+    protected $basepath;
+
+    protected $extension;
+
+    /**
+     * 
+     * 
+     * */
+    protected $pathAliases = [];
+
+    /**
+     * Constructor
+     * 
+     * @param string $basepath
+     * @param string $extension
+     * */
+    public function __construct($basepath, $extension = 'php')
     {
-        $this->path = $path;
+        $this->basepath = $basepath;
 
         $this->extension = $extension;
     }
 
+    /**
+     * Creates a view
+     * 
+     * @param string $view 
+     * @param array $data
+     * @return PHPLegends\View\View
+     * */
     public function create($view, $data = [])
     {
-        return new View($this->buildFilename($view), $data, $this->path);
+        return new View($this->buildFilename($view), $data);
     }
 
+    /**
+     * Build the filename of view
+     * 
+     * @param string $view
+     * @return string
+     * */
     protected function buildFilename($view)
     {
-        return $view . '.' . $this->extension;
+        $filename = $this->parsePathAlias($view) . '.' . $this->extension;
+
+        if ($this->basepath)
+        {
+            $filename = $this->basepath . '/' . $filename;
+        }
+
+        return $filename;
+    }
+
+    /**
+     * Creates a view instance
+     * 
+     * @param string $view
+     * @param array $data
+     * @return PHPLegends\View\View
+     * */
+    public function __invoke($view, $data = [])
+    {
+        return $this->create($view, $data);
+    }
+
+
+    public function setPathAlias($alias, $path)
+    {
+        $this->pathAliases[$alias] = $path;
+
+        return $this;
+    }
+
+    public function getPathAlias($alias)
+    {
+        if (! isset($this->pathAliases[$alias]))
+        {
+            throw new \InvalidArgumentException(
+                "The alias '$alias' is not defined"
+            );
+        }
+
+        return $this->pathAliases[$alias];
+    }
+
+    protected function parsePathAlias($path)
+    {
+        if (strpos($path, ':') === false)
+        {
+            return $path;
+        }
+
+        list($alias, $end) = explode(':', $path, 2);
+
+        return $this->getPathAlias($alias) . '/' . ltrim($end, '/');
     }
 }
