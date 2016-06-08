6150ea860003b07296bd967eb143b98473a4ee7c
Wallace de Souza Vizerra <wallacemaxters@gmail.com>
2016-06-08 12:33:31 -0300

    Removing unecessary methods in Interface

--- src/FinderInterface.php
+++ src/FinderInterface.php
@@ -13,46 +13,4 @@ interface FinderInterface
      * */
     public function find($view);
 
-    /**
-     * 
-     * @param string $name
-     * @param string $path
-     * */
-    public function setPathAlias($name, $path);
-
-    /**
-     * 
-     * @param string $name
-     * @return string
-     * @throws \InvalidArgumentException
-     * */
-    public function getPathAlias($name);
-
-    /**
-     * 
-     * @param string $name
-     * @param string|null $preprocessor
-     * */
-    public function addExtension($extension, $preprocessor = null);
-
-    /**
-     * 
-     * @return array
-     * */
-
-    public function getExtensions();
-
-    /**
-     * 
-     * @param string $path
-     * */
-
-    public function setDefaultPath($path);
-
-    /**
-     * @return string
-     * */
-
-    public function getDefaultPath();
-
 }
