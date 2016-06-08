d22fe15b263454d5c95f8ffce0cf5fb2de9e5662
Wallace de Souza <wallacemaxters@gmail.com>
2016-06-07 21:43:54 -0300

    Removing unecessary methods in contracts

--- src/FinderInterface.php
+++ src/FinderInterface.php
@@ -6,53 +6,11 @@ interface FinderInterface
 {
 
     /**
+     * Find the view
      * 
      * @param string $view
      * @return string
      * @throws PHPLegends\Views\Exceptions\ViewNotFoundException
      * */
     public function find($view);
-
-    /**
-     * 
-     * @param string $name
-     * @param string $path
-     * */
-    public function setPathAlias($name, $path);
-
-    /**
-     * 
-     * @param string $name
-     * @return string
-     * @throws \InvalidArgumentException
-     * */
-    public function getPathAlias($name);
-
-    /**
-     * 
-     * @param string $name
-     * @param string|null $preprocessor
-     * */
-    public function addExtension($extension, $preprocessor = null);
-
-    /**
-     * 
-     * @return array
-     * */
-
-    public function getExtensions();
-
-    /**
-     * 
-     * @param string $path
-     * */
-
-    public function setDefaultPath($path);
-
-    /**
-     * @return string
-     * */
-
-    public function getDefaultPath();
-
 }
