1ef594ed7f5f210cd0792cf723116eafbc3e961f
Wallace de Souza Vizerra <wallacemaxters@gmail.com>
2017-01-19 14:50:15 -0200

    new tests

--- test/FactoryTest.php
+++ test/FactoryTest.php
@@ -1,7 +1,7 @@
 <?php
 
+use PHPLegends\View\Data;
 use PHPLegends\View\Finder;
-
 use PHPLegends\View\Context;
 use PHPLegends\View\Factory;
 
@@ -36,6 +36,25 @@ class FactoryTest extends PHPUnit_Framework_TestCase
 
 		$this->assertTrue($view->getContext()->hasParentView());
 
+
+
+	}
+
+	public function testContextWithInclude()
+	{
+		$view = $this->factory->create('home/include');
+
+		$view->render(); // or called on View cast to string
+
+		$this->assertTrue($view->hasContext());
+
+		$this->assertTrue($view->getContext()->hasParentView());
+
+		$this->assertEquals(
+			'Wallace Maxters', 
+			$view->getContext()->getSectionCollection()->get('child')->getContent()
+		);
+
 	}
 
 	public function testToString()
@@ -45,14 +64,44 @@ class FactoryTest extends PHPUnit_Framework_TestCase
 		$this->assertTrue(method_exists($view, '__toString'));
 	}
 
-	public function testData()
+	public function testDataWhenArrayPassed()
 	{
-		$view = $this->factory->create('home/child');
+		// Teste with Array
+
+		$this->factory->share('global', 'this is global');
+
+		$view = $this->factory->create('home/child', ['age' => 25]);
 
 		$view['name'] = 'Wallace';
 
 		$this->assertEquals('Wallace', $view['name']);
 
 		$this->assertEquals('Wallace', $view->getData()->get('name'));
+
+		$this->assertEquals(25, $view['age']);
+
+		$this->assertEquals('this is global', $view['global']);
+
 	}
+
+	public function testDataWhenDataObjectPassed()
+	{
+		$data = new Data(['age' => 18]);
+
+		$this->factory->share('global', 'i am global baby');
+
+		$view = $this->factory->create('home/child', $data);
+
+		$view['name'] = 'Wayne';
+
+		$this->assertEquals('Wayne', $view['name']);
+
+		$this->assertEquals('Wayne', $view->getData()->get('name'));
+
+		$this->assertEquals(18, $view['age']);
+
+		$this->assertEquals('i am global baby', $view['global']);
+	}
+
+
 }
