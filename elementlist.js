57c607b7acad0c6dff2953ca1917fd8c5aadfc9c
Wallace de Souza Vizerra <wallacemaxters@gmail.com>
2017-01-20 14:08:57 -0200

    first commit

---
+++ elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","PHPLegends\\View\\Context"],["c","PHPLegends\\View\\Data"],["c","PHPLegends\\View\\Exceptions\\ViewNotFoundException"],["c","PHPLegends\\View\\Factory"],["c","PHPLegends\\View\\FactoryInterface"],["c","PHPLegends\\View\\Finder"],["c","PHPLegends\\View\\FinderInterface"],["c","PHPLegends\\View\\PreprocessorInterface"],["c","PHPLegends\\View\\Section"],["c","PHPLegends\\View\\SectionCollection"],["c","PHPLegends\\View\\View"]];
--- src/Exceptions/ViewNotFoundException.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-namespace PHPLegends\View\Exceptions;
-
-class ViewNotFoundException extends \RuntimeException {}
